f8a994f7b749af17770e906d900ffbb67f8e0097
wangjun <2209119121@qq.com>
2019-03-22 14:19:50 +0800

    Initial commit

---
+++ app/src/main/java/com/xrd/webtojs/WebActivity.java
@@ -0,0 +1,105 @@
+package com.xrd.webtojs;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class WebActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        mWebView = (WebView) findViewById(R.id.wb);
+        bt = (Button) findViewById(R.id.bt);
+        init();
+    }
+
+    @SuppressLint("JavascriptInterface")
+    private void init() {
+        WebSettings webSettings = mWebView.getSettings();
+
+        // 设置与Js交互的权限
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setDomStorageEnabled(true);
+
+        // 通过addJavascriptInterface()将Java对象映射到JS对象
+        //参数1：Javascript对象名
+        //参数2：Java对象名
+        mWebView.addJavascriptInterface(new AndroidtoJs(), "test");//AndroidtoJS类对象映射到js的test对象
+
+        // 由于设置了弹窗检验调用结果,所以需要支持js对话框
+        // webview只是载体，内容的渲染需要使用webviewChromClient类去实现
+        // 通过设置WebChromeClient对象处理JavaScript的对话框
+        //设置响应js 的Alert()函数
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                AlertDialog.Builder b = new AlertDialog.Builder(WebActivity.this);
+                b.setTitle("Alert");
+                b.setMessage(message);
+                b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                b.setCancelable(false);
+                b.create().show();
+                return true;
+            }
+
+        });
+        // 加载JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        mWebView.loadUrl("file:///android_asset/javascript.html");
+        bt.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                // Android版本变量
+                final int version = Build.VERSION.SDK_INT;
+                // 因为该方法在 Android 4.4 版本才可使用，所以使用时需进行版本判断
+                if (version < 18) {
+                    mWebView.loadUrl("javascript:showAlert()");
+                } else {
+                    mWebView.evaluateJavascript("javascript:showAlert()", new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String value) {
+                            //此处为 js 返回的结果
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    class AndroidtoJs {
+        public AndroidtoJs(){
+
+        }
+        @JavascriptInterface
+        public void hello(String content){
+            Toast.makeText(WebActivity.this, content, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+}
